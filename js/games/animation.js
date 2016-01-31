d9eabdc311cc17aa06ecf81d41791c3bf259b3c3
Hugo Roque <hugolnx@gmail.com>
2016-01-30 22:26:47 -0200

    animation chain for earth

--- js/games/animation.js
+++ js/games/animation.js
@@ -1,13 +1,16 @@
 (function(namespace) {
   var Callbacks = LNXCommons.CallbackHelper;
 
-  namespace.Animation = function(sprite, images) {
+  namespace.Animation = function(sprite, images, texture) {
     var callbacks = Callbacks.initializeFor(this);
-    var texture = sprite.texture;
+    if(!texture) {
+      texture = sprite.texture;
+    }
     var currentImageIndex = 0;
     var framesOfCurrentImage = images[currentImageIndex].duration;
 
     this.toNextFrame = function(x, y) {
+      sprite.texture = texture;
       var image = images[currentImageIndex];
       texture.frame = new PIXI.Rectangle(image.x, image.y, image.width, image.height);
       framesOfCurrentImage -= 1;
@@ -18,7 +21,8 @@
       sprite.x = x;
       sprite.y = y;
 
-      if(currentImageIndex == images.length - 1) {
+      if(currentImageIndex === images.length - 1) {
+        currentImageIndex = 0;
         callbacks.emit("animationEnd");
       }
     };
